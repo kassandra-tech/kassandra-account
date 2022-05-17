b66d42fba37b7218036e7e772b74bf102f184878
Mike <mikeblack25@gmail.com>
2022-05-17 14:59:57 -0700

    Add Header and Screen Placeholders
    - Add Header to existing screens.
    - Add placeholder screens for Home and Coming Soon.

--- ClientApp/src/screens/log-in/login.js
+++ ClientApp/src/screens/log-in/login.js
@@ -2,11 +2,35 @@ Moralis.start({ serverUrl, appId });
 
 async function login(username, password) {
   try {
-    const user = await Moralis.User.logIn(username, password);
-    alert("You are logged in");
+    await Moralis.User.logIn(username, password);
+    checkStatus(message);
+    window.location.href = "../home/home.html";
   } catch (error) {
-    alert("Error: " + error.code + " " + error.message);
+    checkStatus(message);
   }
+}
+
+async function updatePasswordMessage() {
+  var password = document.getElementById("password-field").value;
+  var passwordMessage = document.getElementById("password-message-label");
+
+  passwordMessage.innerHTML = "";
+
+  if (password.length > 0 && password.length < 8) {
+    passwordMessage.innerHTML = "Password must be longer than 7 characters";
+  }
+}
+
+async function checkStatus(message) {
+  clearMessages();
+  updateUsernameMessage(message);
+  updatePasswordMessage();
+}
+
+async function clearMessages()
+{
+  document.getElementById("username-message-label").innerHTML = "";
+  document.getElementById("password-message-label").innerHTML = "";
 }
 
 document.getElementById("submit-button").onclick = () => login(document.getElementById("username-field").value, document.getElementById("password-field").value);
